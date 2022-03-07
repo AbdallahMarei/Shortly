5d2eb635b39c9015bfa5caa35cd410511d807b18
AbdallahMarei <abdullahmari@gmail.com>
2022-03-07 18:59:17 +0300

    edit

--- script.js
+++ script.js
@@ -3,62 +3,53 @@ let urlInput = document.querySelector(".input-link");
 let showError = document.querySelector(".show-url-error");
 let showLinks = document.getElementById("show-links");
 let showErrorMobile = document.querySelector(".show-url-error-mobile");
-let checkEmailRegex= /^(http[s]?:\/\/(www\.)?|ftp:\/\/(www\.)?|www\.){1}([0-9A-Za-z-\.@:%_\+~#=]+)+((\.[a-zA-Z]{2,3})+)(\/(.)*)?(\?(.)*)?/g;
+let checkEmailRegex = /^(http[s]?:\/\/(www\.)?|ftp:\/\/(www\.)?|www\.){1}([0-9A-Za-z-\.@:%_\+~#=]+)+((\.[a-zA-Z]{2,3})+)(\/(.)*)?(\?(.)*)?/g;
 
-window.onload = ()=>{
-    if(localStorage.getItem("short-links")){
-        showShortLinks();
-    };
-};
-
-shortForm.addEventListener("submit", function(e){
-    e.preventDefault();
-    if(!urlInput.value.match(checkEmailRegex)){
-        urlEnteredIncorreclty("Please Enter a valid url");
-        return
-    } 
-    urlShort(urlInput.value);  
-});
-
-
-const urlShort = async(url) => {
+/**
+ * Shorten url entered by the user
+ * @param {string} url url from user to shorten
+ * @returns stops function if the url is incorrect
+ */
+const shortenUrl = async (url) => {
     const res = await fetch(`https://www.shareaholic.com/v2/share/shorten_link?apikey=8943b7fd64cd8b1770ff5affa9a9437b&url=${url}`);
     const result = await res.json();
-    if(!localStorage.getItem("short-links")){
+    if (!localStorage.getItem("short-links")) {
         let shortLinks = [];
-        let newObjUrl = {
+        var newObjUrl = {
             link: urlInput.value,
             id: new Date(),
             shortLink: result.data
         };
         shortLinks.push(newObjUrl);
-        localStorage.setItem("short-links",JSON.stringify(shortLinks));
+        localStorage.setItem("short-links", JSON.stringify(shortLinks));
     } else {
         let flag = false;
         let shortLinksLocal = JSON.parse(localStorage.getItem("short-links"));
         shortLinksLocal.forEach(link => {
-            if(link.link == urlInput.value){
+            if (link.link == urlInput.value) {
                 flag = true;
             }
         })
-        if(flag){
+        if (flag) {
             urlEnteredIncorreclty("url already entered");
             return
         }
-        let newObjUrl = {
+        var newObjUrl = {
             link: urlInput.value,
             id: new Date(),
             shortLink: result.data
         };
         shortLinksLocal.push(newObjUrl);
-        localStorage.setItem("short-links",JSON.stringify(shortLinksLocal));
+        localStorage.setItem("short-links", JSON.stringify(shortLinksLocal));
     }
     urlEnteredSuccessfully();
-    showShortLinks();
+    displayShortenedLinks(newObjUrl);
 }
 
-
-function urlEnteredSuccessfully(){
+/**
+ * Displays a span underneath the input with a success message
+ */
+function urlEnteredSuccessfully() {
     urlInput.style.borderColor = "green";
     showError.innerHTML = "Url shortened successfully";
     showError.style.color = "#2bd0d0";
@@ -66,8 +57,11 @@ function urlEnteredSuccessfully(){
     showErrorMobile.style.color = "#2bd0d0";
 };
 
-
-function urlEnteredIncorreclty (msg){
+/**
+ * Displays a span underneath the input with a failed message
+ * @param {string} msg message to be displayed beneath input if the input was entered incorrectly
+ */
+function urlEnteredIncorreclty(msg) {
     showError.innerHTML = msg;
     showErrorMobile.innerHTML = msg;
     showErrorMobile.style.color = "red"
@@ -76,35 +70,60 @@ function urlEnteredIncorreclty (msg){
     urlInput.value = "";
 };
 
+/**
+ * Displays the short links that user inputted
+ * @param {object} obj object that contains the lonk url and the short url
+ */
+function displayShortenedLinks(obj) {
+    showLinks.insertAdjacentHTML("afterend", `<div class="short-links-div">
+        <span class="show-links-div-link">${obj.link}</span>
+        <hr />
+        <div class="individualShortLink">
+          <a href="${obj.shortLink}" target="_blank">${obj.shortLink}</a>
+          <button class="copy-link">Copy</button>
+        </div>
+      </div>`)
+};
 
-function showShortLinks(){
-    let html = `<div>`;
-    let shortLinksLocalStorage = JSON.parse(localStorage.getItem("short-links")).reverse();
-    shortLinksLocalStorage.forEach(link => {
-        html += `<div class="short-links-div">
-        <span class="link">${link.link}</span>
+/**
+ * Displays all the links from local storage on page load
+ */
+function displayShortenedLinksOnLoad() {
+    let linksFromLocal = JSON.parse(localStorage.getItem("short-links"));
+    linksFromLocal.forEach(link => {
+        showLinks.insertAdjacentHTML("afterend", `<div class="short-links-div">
+        <span class="show-links-div-link">${link.link}</span>
         <hr />
         <div class="individualShortLink">
           <a href="${link.shortLink}" target="_blank">${link.shortLink}</a>
           <button class="copy-link">Copy</button>
         </div>
-      </div>`
-    });
-    html+=`</div>`;
-    showLinks.innerHTML = html;
+      </div>`)
+    })
 };
 
 
-showLinks.addEventListener("click", function(e){
-    if(e.target.classList.contains("copy-link")){
+window.onload = () => {
+    if (localStorage.getItem("short-links")) {
+        displayShortenedLinksOnLoad();
+    };
+};
+
+shortForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    if (!urlInput.value.match(checkEmailRegex)) {
+        urlEnteredIncorreclty("Please Enter a valid url");
+        return
+    }
+    shortenUrl(urlInput.value);
+});
+
+showLinks.addEventListener("click", function (e) {
+    if (e.target.classList.contains("copy-link")) {
         e.target.innerText = 'Copied';
         e.target.style.background = 'green';
         let copiedLink = e.target.parentElement.firstElementChild.href;
         navigator.clipboard.writeText(copiedLink);
-        setTimeout(() => {
-            e.target.innerText = "Copy";
-            e.target.style.background = '#2bd0d0';
-        }, 4000);
     };
 });
 
